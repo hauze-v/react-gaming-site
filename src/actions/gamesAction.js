d27e78106c445da0727131366c1820b601282fa3
Adam Hauze <hauze_v@protonmail.com>
2021-01-11 07:38:50 -0500

    Fetched some additional data using Reducer and Action files.

--- src/actions/gamesAction.js
+++ src/actions/gamesAction.js
@@ -1,14 +1,18 @@
 import axios from "axios";
-import { popularGamesURL } from "../api";
+import { popularGamesURL, upcomingGamesURL, newGamesURL } from "../api";
 
 // Action Creator (use another arrow function)
 export const loadGames = () => async (dispatch) => {
   // FETCH WITH AXIOS
   const popularGamesData = await axios.get(popularGamesURL());
+  const upcomingGamesData = await axios.get(upcomingGamesURL());
+  const newGamesData = await axios.get(newGamesURL());
   dispatch({
     type: "FETCH_GAMES",
     payload: {
       popular: popularGamesData.data.results,
+      upcoming: upcomingGamesData.data.results,
+      newGames: newGamesData.data.results,
     },
   });
 };
