52ebeb492eae4c60bbfffb50a9b2e41fa0ef0c05
Adam Hauze <hauze_v@protonmail.com>
2021-01-14 08:03:14 -0500

    Implemented cool Redux trick to wait for data to load before rendering.

--- src/actions/detailAction.js
+++ src/actions/detailAction.js
@@ -3,6 +3,11 @@ import { gameDetailsURL, gameScreenshotsURL } from "../api";
 
 // Create action
 export const loadDetail = (id) => async (dispatch) => {
+  // Redux trick for loading data
+  dispatch({
+    type: "LOADING_DETAIL",
+  });
+
   // Fetch data
   const detailData = await axios.get(gameDetailsURL(id));
   const screenshotData = await axios.get(gameScreenshotsURL(id));
--- src/components/GameDetail.js
+++ src/components/GameDetail.js
@@ -5,38 +5,43 @@ import { motion } from "framer-motion";
 import { useSelector } from "react-redux";
 
 const GameDetail = () => {
-  // Data
-  const { screenshots, game } = useSelector((state) => state.detail);
+  // Grab data from state
+  const { screenshots, game, isLoading } = useSelector((state) => state.detail);
+
   return (
-    <CardShadow>
-      <Detail>
-        <Stats>
-          <div className="rating">
-            <h3>{game.name}</h3>
-            <p>Rating: {game.rating}</p>
-          </div>
-          <Info>
-            <h3>Platforms</h3>
-            <Platforms>
-              {game.platforms.map((data) => (
-                <h3 key={data.platform.id}>{data.platform.name}</h3>
+    <>
+      {!isLoading && (
+        <CardShadow>
+          <Detail>
+            <Stats>
+              <div className="rating">
+                <h3>{game.name}</h3>
+                <p>Rating: {game.rating}</p>
+              </div>
+              <Info>
+                <h3>Platforms</h3>
+                <Platforms>
+                  {game.platforms.map((data) => (
+                    <h3 key={data.platform.id}>{data.platform.name}</h3>
+                  ))}
+                </Platforms>
+              </Info>
+            </Stats>
+            <Media>
+              <img src={game.background_image} alt="background" />
+            </Media>
+            <Description>
+              <p>{game.description_raw}</p>
+            </Description>
+            <div className="gallery">
+              {screenshots.results.map((screen) => (
+                <img src={screen.image} key={screen.id} alt="screenshot" />
               ))}
-            </Platforms>
-          </Info>
-        </Stats>
-        <Media>
-          <img src={game.background_image} alt="background" />
-        </Media>
-        <Description>
-          <p>{game.description_raw}</p>
-        </Description>
-        <div className="gallery">
-          {screenshots.results.map((screen) => (
-            <img src={screen.image} key={screen.id} alt="screenshot" />
-          ))}
-        </div>
-      </Detail>
-    </CardShadow>
+            </div>
+          </Detail>
+        </CardShadow>
+      )}
+    </>
   );
 };
 
