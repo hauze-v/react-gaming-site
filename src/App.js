7e7b344141c5178a921fa6fc6ca4005e83428f86
Adam Hauze <hauze_v@protonmail.com>
2021-01-16 12:42:27 -0500

    Added nav bar and styled it a bit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React from "react";
 // Components and Pages
 import Home from "./pages/Home";
+import Nav from "./components/Nav";
 // Router
 import { Route } from "react-router-dom";
 // Styles
@@ -10,6 +10,7 @@ function App() {
   return (
     <div className="App">
       <GlobalStyles />
+      <Nav />
       <Route path={["/game/:id", "/"]}>
         <Home />
       </Route>
